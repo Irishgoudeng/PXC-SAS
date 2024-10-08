20914f7812d6fc08f17840992fa769dd7050bbb8
GMJSilmaro <dzheysilmaro@gmail.com>
2024-10-08 11:45:22 +0800

    Fix an issue on Import trace for ./styles/theme.scss > Build failed because of webpack errors

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { Fragment, useEffect } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
-// import theme style scss file
-import '/styles/theme.scss';  
+
 
 const Home = () => {
 
